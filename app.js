9890c7003657e869ce41db2e58d09d4fea0014d0
Tyler Jurecki <jurecki07@yahoo.com>
2017-09-06 15:23:53 -0500

    app setup

--- app.js
+++ app.js
@@ -4,25 +4,32 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoskin = require('mongoskin');
+///////
 
+
+///////Routes
 var index = require('./routes/index');
 var users = require('./routes/users');
-
+/////Express app
 var app = express();
+/////// Database
+var db = mongoskin.db('mongodb://@localhost:27017/test', {safe:true})
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+
+
+
 app.use('/users', users);
 
 // catch 404 and forward to error handler
